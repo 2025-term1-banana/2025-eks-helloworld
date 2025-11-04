dbd233ea93086d0dc8d12e94b8e0b92a8b48ea93
FunnyKim <funny@FunnyKims-MacBook-Pro.local>
2025-11-04 14:12:22 +0900

    inti

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, Kubernetes World!"
+
+if __name__ == '__main__':
+    # host='0.0.0.0'로 설정하여 컨테이너 외부에서 접근 가능하도록 합니다.
+    # port=5000 (또는 원하는 포트)로 실행합니다.
+    app.run(host='0.0.0.0', port=5000)
