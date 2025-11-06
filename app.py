c033810bce0bfbce86c1c55b03973adf5b7d18c6
FunnyKim <funny@FunnyKims-MacBook-Pro.local>
2025-11-07 02:10:01 +0900

    test

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, Kubernetes World"
 
+@app.route("/healthz")
+def healthz():
+    return jsonify(version=os.getenv("APP_VERSION", "unknown"))
+
 if __name__ == '__main__':
     # host='0.0.0.0'로 설정하여 컨테이너 외부에서 접근 가능하도록 합니다.
     # port=5000 (또는 원하는 포트)로 실행합니다.
