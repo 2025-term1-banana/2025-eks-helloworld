05c5a010efd3ce26ddfdb48cfb72af51a3811891
FunnyKim <funny@FunnyKims-MacBook-Pro.local>
2025-11-07 11:18:31 +0900

    rollback test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Kubernetes World - canary test!"
+    return "Hello, Kubernetes World - New Version! (before rollback)"
 
 @app.route("/healthz")
 def healthz():
