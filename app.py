faae88aeefcfe0e8d54dfd0fcfea9e507680b05d
Gyeongsik Kim <gskim@smins.co.kr>
2025-11-07 23:11:23 +0900

    db test

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
+from json import loads
+from os import getenv
+import boto3
+from botocore.exceptions import ClientError
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def get_secret():
+    secret_name = "prod/hackaton/db"
+    region_name = "ap-northeast-1"
+
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+
+    secret: str = get_secret_value_response["SecretString"]
+    return loads(secret)
+
+
+@app.route("/")
 def hello():
-    return "Hello, Kubernetes World - New Version! (rollback!!!)"
+    secret = get_secret()
+    return f"Hello, Kubernetes World - New Version! (rollback!!!) {secret["flask"]}"
+
 
 @app.route("/healthz")
 def healthz():
-    return jsonify(version=os.getenv("APP_VERSION", "unknown"))
+    return jsonify(version=getenv("APP_VERSION", "unknown"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # host='0.0.0.0'로 설정하여 컨테이너 외부에서 접근 가능하도록 합니다.
     # port=5000 (또는 원하는 포트)로 실행합니다.
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
