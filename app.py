568a390a34b2310ec5f26d8470cd1ed51622f6ac
FunnyKim <funny@FunnyKims-MacBook-Pro.local>
2025-11-07 18:03:44 +0900

    newest

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Kubernetes World - New Version! (before rollback)"
+    return "Hello, Kubernetes World - New Version! (after newest)"
 
 @app.route("/healthz")
 def healthz():
