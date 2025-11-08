afe4052231f9976e7decfa2008c278c620832823
Gyeongsik Kim <gskim@smins.co.kr>
2025-11-08 14:48:54 +0900

    db test

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     app.testing = True
@@ -12,3 +13,9 @@ def test_hello(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b"Hello" in response.data
+
+
+def test_db(client):
+    response = client.get("/db")
+    assert response.status_code == 200
+    assert b"not set" in response.data
